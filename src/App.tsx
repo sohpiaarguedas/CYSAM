c1a00e0b726a32c2f59f263f0b4232220eabc897
RicardoPerezJ <ricardo.perezjimenez@ucr.ac.cr>
2026-06-06 10:47:10 -0600

    Prueba

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { MainLayout } from "./layouts/MainLayout";
 import ArticleDetail from "./pages/articleDetail";
+import { Principal } from "./pages/principal";
 
 function App() {
   return (
-    <ArticleDetail
+    
+    <MainLayout>
+      <ArticleDetail
       title="Que hacer si hicieron transacciones sin mi autorización desde mi tarjeta"
       description="Si detectaste movimientos o compras que no reconoces en tu tarjeta"
       video=""
     />
+
+    {/* <Principal /> */}
+    </MainLayout>
   );
 }
 
